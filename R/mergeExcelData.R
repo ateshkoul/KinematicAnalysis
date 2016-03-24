2cec72bcdf0f0c874756c0c8935e28a72b1f8b31
Atesh <atesh.koul@gmail.com>
2016-03-24 15:19:56 +0100

    updated mergeExcelData.R

--- R/mergeExcelData.R
+++ R/mergeExcelData.R
@@ -1,31 +1,16 @@
 #' Read Data from multiple files/sheets of Excel files and create a single dataframe
 #'
-#'
+#' \code{mergeExcelData} takes in multiple filenames/single excel file with multiple sheets and merges as
+#' a single dataframe.
+#' uses packages: openxlsx
 #'@author
 #'Atesh Koul, RBCS, Istituto Italiano di technologia
 #'
 #'\email{atesh.koul@@gmail.com}
 #'The function assumes that the files to be merged have the same number of columns
 #'(necessity for rbind)
-# Previous avatar:
-# readBehavData <- function(SheetName,SheetNo){
-#   if(!require(openxlsx)){install.packages('openxlsx')}
-# 
-#   require("openxlsx")
-#   nSheets <- length(SheetNo)
-# 
-#   Data <- list()
-#   for(i in 1:nSheets){
-#     DataSheet <- read.xlsx(xlsxFile = SheetName,sheet = SheetNo[i])
-#     Data <- rbind(Data,DataSheet)
-# 
-#   }
-#   return(Data)
-# 
-# 
-# 
-# }
-mergeExcelData <- function (filenames, SheetNo,selectedData = NULL,...) 
+
+mergeExcelData <- function (filenames, SheetNo,selectedData = NULL,...)
 {
   if (!require(openxlsx)) {
     install.packages("openxlsx")
@@ -36,9 +21,9 @@ mergeExcelData <- function (filenames, SheetNo,selectedData = NULL,...)
     # good to know which files u are merging
     print(filename)
     excel = loadWorkbook(filename)
-    
+
     # take care if u input sheet names instead of sheet numbers
-    # even though when it says SheetNo :) 
+    # even though when it says SheetNo :)
     if(!is.double(SheetNo)){
       print('SheetNo is not a number')
       Sheets = SheetNo
@@ -46,7 +31,7 @@ mergeExcelData <- function (filenames, SheetNo,selectedData = NULL,...)
       Sheets = names(excel)
       Sheets = Sheets[SheetNo]
     }
-    
+
     nSheets <- length(Sheets)
     for (sheetName in Sheets) {
       DataSheet <- readWorkbook(excel, sheet = sheetName,...)
